d6e93e5583735bd8c789eb6ecedea50602acdd37
KerryTang <sunrain81928@gmail.com>
2016-07-02 15:45:21 +0800

    [Update] Week 2 - Implement exists with forall

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -79,14 +79,7 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-        if (a > bound) false
-        else if (!contains(s, a) || !p(a)) iter(a+1)
-        else true
-      }
-    iter(-bound)
-  }
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
 
 /**
  * Returns a set transformed by applying `f` to each element of `s`.
